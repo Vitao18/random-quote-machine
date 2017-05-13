5fe481e02454111e64094f358c9d585cfc8a1a2c
Vitao18 <victoronline9@gmail.com>
2017-05-13 12:57:10 -0300

    added js

---
+++ scripts.js
@@ -0,0 +1,10 @@
+var quoteList = ["I am coming more and more to the conviction that the necessity of our geometry cannot be demonstrated, at least neither by, nor for, the human intellect.",
+"God does arithmetic", 	"Madam, I have come from a country where people are hanged if they talk.", "We build too many walls and not enough bridges.",
+"Genius is patience."]
+
+var author = ["Carl Friedrich Gauss", "Carl Friedrich Gauss", "Leonhard Euler", "Isaac Newton", "Isaac Newton"]
+
+function randomQuote(){
+	var quote = document.getElementById('quote');
+	quote.innerHTML = quoteList[0];
+}
