d4d406e7f3f6a26f5c66986622b208737c42ee1e
Vitao18 <victoronline9@gmail.com>
2017-05-15 23:33:37 -0300

    added more quotes

--- scripts.js
+++ scripts.js
@@ -3,9 +3,11 @@
 
 var quoteList = ["I am coming more and more to the conviction that the necessity of our geometry cannot be demonstrated, at least neither by, nor for, the human intellect.",
 "God does arithmetic", 	"Madam, I have come from a country where people are hanged if they talk.", "We build too many walls and not enough bridges.",
-"Genius is patience."]
+"Genius is patience.", "Mathematicians are like managers - they want improvement without change.", "Need we add that mathematicians themselves are not infallible?",
+"It would be better for the true physics if there were no mathematicians on earth."]
 
-var authorList = ["Carl Friedrich Gauss", "Carl Friedrich Gauss", "Leonhard Euler", "Isaac Newton", "Isaac Newton"]
+var authorList = ["Carl Friedrich Gauss", "Carl Friedrich Gauss", "Leonhard Euler", "Isaac Newton", "Isaac Newton", "Edsger Dijkstra", "Henri Poincare"
+, "Daniel Bernoulli"]
 
 function generate(min, max){
   return Math.floor(Math.random() * (max - min)) + min;
